e9aa89937a03f4d4e2d9630cc2996f5fdcd8dd66
Jennifer Nordell <jennifer.nordell@outlook.com>
2018-11-10 03:46:54 +0100

    addition of data.js file and some work done on starting the game

---
+++ js/data.js
@@ -0,0 +1,17 @@
+let phrases = [
+  "when pigs fly",
+  "over the moon",
+  "in hot water"
+];
+
+const winningMessages = [
+  "Terrific",
+  "Spectacular",
+  "Awesome"
+];
+
+let losingMessages = [
+  "Too bad",
+  "Try again",
+  "Better luck next time"
+];
